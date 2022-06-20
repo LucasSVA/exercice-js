6ee1ce248fb9abb2b8c813c481baad788dc6301a
LucasSVA <lucas.svanadze@gmail.com>
2022-06-20 16:48:20 +0200

    exercise about number

---
+++ number.js
@@ -0,0 +1,62 @@
+// ----- ex -1
+
+// const integer = 102
+// const float = 13.9
+// console.log(`I've two numbers ${integer} and ${float}`);
+
+
+// ------ ex - 2
+
+// const basic = 34
+// const stringfied = basic.toString()
+
+// console.log(stringfied);
+
+
+// ---- ex -3
+
+
+
+// --- ex -4
+
+// const test = 12
+// const bis = 5
+// console.log(test + bis)
+// console.log(test - bis);
+// console.log(test * bis);
+// console.log(test / bis);
+// console.log(test % bis);
+
+// ---- ex 5
+
+// console.log(999 % 1)     (resultat = 0)
+// console.log(999 % 2)
+// console.log(999 % 3)     (resultat = 0)
+// console.log(999 % 4)
+// console.log(999 % 5)
+
+
+// console.log(25 % 1)   (resultat = 0)
+// console.log(25 % 2)
+// console.log(25 % 3)
+// console.log(25 % 4)
+// console.log(25 % 5)    (resultat = 0)
+
+
+// console.log(190280 % 1)  (resultat = 0)
+// console.log(190280 % 2)  (resultat = 0)
+// console.log(190280 % 3)
+// console.log(190280 % 4)  (resultat = 0)
+// console.log(190280 % 5)
+
+// console.log(47 % 1)  (resultat = 0)
+// console.log(47 % 2)
+// console.log(47 % 3)
+// console.log(47 % 4)
+// console.log(47 % 5)
+
+// console.log(691 % 1)  (resultat = 0)
+// console.log(691 % 2)
+// console.log(691 % 3)
+// console.log(691 % 4)
+// console.log(691 % 5)
