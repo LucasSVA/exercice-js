e488495fe91d0ccf9fcf50bc2893daf02d5e50af
LucasSVA <lucas.svanadze@gmail.com>
2022-06-20 12:36:45 +0200

    6 basic exercices done

---
+++ index.js
@@ -0,0 +1,44 @@
+//  ----  ex-1
+// console.log("Hello world");
+
+// ------ ex-2
+
+// const sentence = "My name is Lucas"
+// console.log(sentence);
+
+// ----- ex-3
+
+// const name = "Lucas"
+// console.log(`Nice to meet you ${name}`);
+
+
+// ----- ex-4
+
+// const sentence = "I'm a new text !"
+// console.log(sentence.length);
+
+
+// ---- ex-5
+
+const food = "croissant is meh"
+console.log(food.replace("meh", "so good"));
+
+
+// ----- ex-6
+
+const basic = "This is good"
+const basicUp = basic.toLocaleUpperCase()
+console.log(basicUp);
+
+// ----- ex-7
+
+const word = "banana"
+const letters = word.split("")
+console.log(letters);
+
+// ---- ex -8
+
+const age = "36"
+const template = `i'm ${age} years old`
+
+console.log(template);
