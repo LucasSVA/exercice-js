5402a8b26a54f36c00af50824c8b692fa4801995
LucasSVA <lucas.svanadze@gmail.com>
2022-06-20 14:02:48 +0200

    a few exercice done for bonus about string methods

--- index.js
+++ index.js
@@ -20,25 +20,94 @@
 
 // ---- ex-5
 
-const food = "croissant is meh"
-console.log(food.replace("meh", "so good"));
+// const food = "croissant is meh"
+// console.log(food.replace("meh", "so good"));
 
 
 // ----- ex-6
 
-const basic = "This is good"
-const basicUp = basic.toLocaleUpperCase()
-console.log(basicUp);
+// const basic = "This is good"
+// const basicUp = basic.toLocaleUpperCase()
+// console.log(basicUp);
 
 // ----- ex-7
 
-const word = "banana"
-const letters = word.split("")
-console.log(letters);
+// const word = "banana"
+// const letters = word.split("")
+// console.log(letters);
 
 // ---- ex -8
 
-const age = "36"
-const template = `i'm ${age} years old`
+// const age = "36"
+// const template = `i'm ${age} years old`
 
-console.log(template);
+// console.log(template);
+
+
+//----- bonus -1
+
+// const firstLesson = "This is my first lesson of JS"
+// let index = 5
+
+// console.log(`the index ${index} corrsponds to letter ${firstLesson.at(index)}`);
+
+// index = -7
+
+// console.log(`the index ${index} corresponds to the letter ${firstLesson.at(index)}`);
+
+
+// ----- bonus-2
+
+
+// const myName = "Lucas"
+
+// let index = 2
+
+// console.log(`the character code ${myName.charCodeAt(index)} corresponds to the letter ${myName.charAt(index)}`);
+
+// const hisName = "Benoit"
+
+// console.log(`the characher code ${hisName.charCodeAt(index)} corresponds to the letter ${hisName.charAt(index)}`);
+
+
+// ---- bonus-3
+
+// const firstName = "Lucas"
+// const familyName = "SVANADZE"
+
+// let about = firstName.concat(` ${familyName}`)
+
+// console.log(about);
+
+
+//          >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// const fruitOne = "banana"
+// const fruitTwo = "apple"
+// const fruitThree = "pear"
+
+// let shoppingList = fruitOne.concat(", ", fruitTwo, " , ", fruitThree)
+
+// shoppingList = fruitOne.concat(` ${fruitTwo}, ${fruitThree}`)
+
+
+// console.log(shoppingList);
+
+
+
+// ----- bonus-4
+
+//const permission = "We are allowed to wear shorts to school!"
+
+// console.log(permission.endsWith("school!", 40));
+
+// console.log(permission.endsWith("allowed", 14));
+
+//console.log(permission.endsWith("forbidden", 14));    (shoud be - false)
+
+
+
+
+
+
+
